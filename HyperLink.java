be0562c6897dd95c10c548f143e8fdf7bea550e9
Kai-Hsiang Chang <kaihsian@usc.edu>
2018-12-02 21:13:33 -0800

    add HyperLink

---
+++ HyperLink.java
@@ -0,0 +1,60 @@
+public class HyperLink {
+	
+	private int x, y, width, height;
+	private String name = null;
+	
+	HyperLink(int x, int y, int width, int height){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	HyperLink(int x, int y, int width, int height, String name){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.name = name;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
